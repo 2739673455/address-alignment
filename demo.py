799841f3fed9e77d75e075f2b0edf06629e4d370
kodey <k2739673455@gmail.com>
2025-09-05 00:11:23 +0800

    夜里更新

--- demo.py
+++
@@ -1,6 +0,0 @@
-import config
-from transformers import BertForTokenClassification, BertModel
-
-# model = BertForTokenClassification.from_pretrained(config.BERT_MODEL)
-model = BertModel.from_pretrained(config.BERT_MODEL)
-print(model)
